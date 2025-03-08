ed96de04f6058e4a3f98edbedacb5f982bf233a7
Cecilia Sanare <ceci@sanare.dev>
2025-03-08 13:47:03 -0600

    ci: improve code coverage detection

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,8 @@ export default defineConfig({
     include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
       reporter: [process.env.CI ? 'text-summary' : 'text', 'lcovonly'],
+      include: ['src/**'],
+      exclude: ['src/types', 'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     },
   },
   plugins: [
